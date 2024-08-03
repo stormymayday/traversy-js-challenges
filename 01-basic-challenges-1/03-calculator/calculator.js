f7db1555b4fc8b69c716da17c7973e874749e3ee
stormymayday <stormymayday7@gmail.com>
2024-08-02 17:42:03 -0700

    calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,29 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+    let result;
+
+    switch (operator) {
+        case "+":
+            // return num1 + num2;
+            result = num1 + num2;
+            break;
+        case "-":
+            // return num1 - num2;
+            result = num1 - num2;
+            break;
+        case "*":
+            // return num1 * num2;
+            result = num1 * num2;
+            break;
+        case "/":
+            // return num1 / num2;
+            result = num1 / num2;
+            break;
+        default:
+            // return null;
+            throw new Error("Invalid operator");
+    }
+
+    return result;
+}
 
 module.exports = calculator;

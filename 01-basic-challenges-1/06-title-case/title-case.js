02efbeaced03c6f3ab2d87f3895060dc63f22615
stormymayday <stormymayday7@gmail.com>
2024-08-07 18:16:37 -0700

    Title Case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,14 @@
-function titleCase() {}
+function titleCase(str) {
+    // console.log(str.split(" "));
+    let strArr = str.toLowerCase().split(" ");
+
+    let newStr = strArr
+        .map((str) => {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+        })
+        .join(" ");
+
+    return newStr;
+}
 
 module.exports = titleCase;

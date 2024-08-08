c6142e49e541140cd937d3cc340450912cb20166
stormymayday <stormymayday7@gmail.com>
2024-08-07 18:24:38 -0700

    Reverse String

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,12 @@
-function reverseString() {}
+function reverseString(str) {
+    let reversedStr = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        // console.log(str[i]);
+        reversedStr += str[i];
+    }
+
+    return reversedStr;
+}
 
 module.exports = reverseString;

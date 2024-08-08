578ebc1c85b0b50b9b26afc7291d633cd9bb2252
stormymayday <stormymayday7@gmail.com>
2024-08-07 17:53:11 -0700

    Count Occurances

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,20 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        // console.log(str[i]);
+        if (str[i] === char) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// Split Method
+// const countOccurrences = (str, char) => {
+//     // console.log(str.split(char).length - 1);
+//     return str.split(char).length - 1;
+// };
 
 module.exports = countOccurrences;

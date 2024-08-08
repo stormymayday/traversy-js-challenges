71de1a21fa370804db9c8e891e78366e94d87d4f
stormymayday <stormymayday7@gmail.com>
2024-08-07 17:59:30 -0700

    Find Max Number

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,20 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+    // let largestNum = 0;
+    let largestNum = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        // console.log(arr[i]);
+        if (arr[i] > largestNum) {
+            largestNum = arr[i];
+        }
+    }
+
+    return largestNum;
+}
+
+// Max method
+// const findMaxNumber = (arr) => {
+//     return Math.max(...arr);
+// };
 
 module.exports = findMaxNumber;

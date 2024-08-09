586faad4e9ea90c5f96194d9cb280c4fe65049a0
stormymayday <stormymayday7@gmail.com>
2024-08-09 11:46:33 -0700

    Palindrome using regex

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,23 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    // Removing non-alphanumeric characters version 1
+    // const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+    // Removing non-alphanumeric characters version 2
+    const formattedStr = str.toLowerCase().replace(/[^\w+]/g, "");
+
+    let reverseStr = "";
+
+    for (let i = formattedStr.length - 1; i >= 0; i--) {
+        reverseStr += formattedStr[i];
+    }
+
+    // console.log(reverseStr);
+
+    if (formattedStr === reverseStr) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 module.exports = isPalindrome;

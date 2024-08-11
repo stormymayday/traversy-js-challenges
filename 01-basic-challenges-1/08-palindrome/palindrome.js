3318caf639457453fb940ba682012c8b68bb7928
stormymayday <stormymayday7@gmail.com>
2024-08-10 19:29:42 -0700

    Palindrome - non-regex

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,23 +1,53 @@
 function isPalindrome(str) {
     // Removing non-alphanumeric characters version 1
     // const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
-
     // Removing non-alphanumeric characters version 2
-    const formattedStr = str.toLowerCase().replace(/[^\w+]/g, "");
+    // let formattedStr = str.toLowerCase().replace(/[^\w+]/g, "");
+    // let reverseStr = "";
+    // for (let i = formattedStr.length - 1; i >= 0; i--) {
+    //     reverseStr += formattedStr[i];
+    // }
+    // return formattedStr === reverseStr;
+    // Regex - END
+    // Non Regex (Helper Functions) - START
 
-    let reverseStr = "";
+    const formattedStr = removeNonAlphaNumeric(str.toLowerCase());
 
-    for (let i = formattedStr.length - 1; i >= 0; i--) {
-        reverseStr += formattedStr[i];
-    }
+    const reversedStr = reverseString(formattedStr);
 
-    // console.log(reverseStr);
-
-    if (formattedStr === reverseStr) {
-        return true;
-    } else {
-        return false;
-    }
+    return formattedStr === reversedStr;
 }
+
+const removeNonAlphaNumeric = (str) => {
+    let formattedStr = "";
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+
+        if (isAlphaNumeric(char)) {
+            formattedStr += char;
+        }
+    }
+
+    return formattedStr;
+};
+
+const isAlphaNumeric = (char) => {
+    const code = char.charCodeAt(0);
+    return (
+        (code >= 48 && code <= 57) || // Numbers 0-9
+        (code >= 97 && code <= 122) // Lowercase a-z
+    );
+};
+
+const reverseString = (str) => {
+    let reversedStr = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+
+    return reversedStr;
+};
 
 module.exports = isPalindrome;
